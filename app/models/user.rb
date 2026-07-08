cc19aef9c4c103e6dfbe1677430db5c6f3da3992
erik petrowski <erik.richard.petrowski@gmail.com>
2026-07-08 12:44:50 +0200

    Added chat association to user model, ensured dependencies for user model :)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   # Associations
   has_many :chats, dependent: :destroy
 
+  # Devise stuff :)
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          # for Google OmniAuth
