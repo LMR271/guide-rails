7230b93e660e841f4e05f74a0a4b8b28d4a2a021
erik petrowski <erik.richard.petrowski@gmail.com>
2026-07-09 14:29:57 +0200

    Made buttons more square, made button red uniform, added profile page, created feature where image on homepage and landing page is one of 6 random illustrations.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_08_103746) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_09_115247) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -38,6 +38,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_08_103746) do
     t.string "encrypted_password", default: "", null: false
     t.string "first_name"
     t.string "last_name"
+    t.string "nickname"
     t.string "provider"
     t.datetime "remember_created_at"
     t.datetime "reset_password_sent_at"
